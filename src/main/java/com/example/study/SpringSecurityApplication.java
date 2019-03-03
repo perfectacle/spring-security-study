5d85be19f0d5c01010a9a2323a94457e9766ebe3
perfectacle <perfectacle@gmail.com>
2019-03-04 00:54:20 +0900

    change application name

--- src/main/java/com/example/study/SpringSecurityApplication.java
+++ src/main/java/com/example/study/SpringSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityMvcJavaApplication {
+public class SpringSecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityMvcJavaApplication.class, args);
+        SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
 }
